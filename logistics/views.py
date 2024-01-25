f984af10d6fa01c0f8e9f1767bf6f6b941227a7e
scabrera <scabrera@siu.edu.ar>
2024-01-24 21:25:26 -0300

    Se agrega informacion de user y pass de admin

--- logistics/views.py
+++ logistics/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .models import Paquete, ItemPlanilla
 
 def hello(request):
-    return HttpResponse("<h1>Para ingresar al endpoint con un nro de tracking dado, correr la url api/paquetes/1</h1>")
+    return HttpResponse("<h1>Para ingresar al endpoint con un nro de tracking dado, correr la url api/paquetes/1 <br> En el readme se encuentra usuario y pass de admin</h1>")
 
 @api_view(['GET'])
 def paquete_detail(request, tracking):
